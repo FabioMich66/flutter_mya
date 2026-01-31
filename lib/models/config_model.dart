9ed7f0ad4f2e2cfed6d0232fcc95fe5fb9d02438
FabioMich66 <68987303+FabioMich66@users.noreply.github.com>
2026-01-31 13:49:59 +0100

    Update config_model.dart

--- lib/models/config_model.dart
+++ lib/models/config_model.dart
@@ -2,22 +2,26 @@ class ConfigModel {
   final String uri;
   final String user;
   final String password;
+  final String? token;
 
   ConfigModel({
     required this.uri,
     required this.user,
     required this.password,
+    this.token,
   });
 
-  Map<String, dynamic> toJson() => {
-        'uri': uri,
-        'user': user,
-        'password': password,
-      };
-
-  factory ConfigModel.fromJson(Map<String, dynamic> json) => ConfigModel(
-        uri: json['uri'],
-        user: json['user'],
-        password: json['password'],
-      );
+  ConfigModel copyWith({
+    String? uri,
+    String? user,
+    String? password,
+    String? token,
+  }) {
+    return ConfigModel(
+      uri: uri ?? this.uri,
+      user: user ?? this.user,
+      password: password ?? this.password,
+      token: token ?? this.token,
+    );
+    }
 }
